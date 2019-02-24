3a66ea212accd1b6a0ed78db886a9fc473acc7ca
Jarno Le Conté <jarno@pti.nl>
2019-02-24 19:10:56 +0100

    Setup API namespace and TypeSafe responses.

--- api-service/app.js
+++ api-service/app.js
@@ -1,52 +1,94 @@
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
+const { callbackifyAll } = require('../lib/helpers');
 
 const protoFile = '../protos/api.proto';
 
-const packageDefinition = protoLoader.loadSync(protoFile);
-const { API } = grpc.loadPackageDefinition(packageDefinition).api;
+const protoOptions = {
+  keepCase: false,
+  longs: String,
+  enums: Number,
+  defaults: false,
+  oneofs: false,
+  arrays: true,
+};
+
+const packageDefinition = protoLoader.loadSync(protoFile, protoOptions);
+const { Api } = grpc.loadPackageDefinition(packageDefinition).api;
 
 
 const consumers = [];
-
-function getConsumers(call, callback) {
-  callback(null, { result: consumers });
-}
-
-function getConsumer(call, callback) {
-  const consumer = consumers.find(c => c.id === call.request.id);
-  callback(null, { result: consumer });
-}
-
-function updateConsumer(call, callback) {
-  const consumer = consumers.find(c => c.id === call.request.id) || {};
-  Object.assign(consumer, call.request);
-  callback(null, { result: consumer });
-}
-
-function createConsumer(call, callback) {
-  const id = Math.max(0, ...consumers.map(c => c.id)) + 1;
-  const consumer = { id, ...call.request };
-  consumers.push(consumer);
-  callback(null, { result: consumer });
-}
-
-function removeConsumer(call, callback) {
-  const index = consumers.findIndex(c => c.id === call.request.id);
-  if (index !== -1) consumers.splice(index, 1);
-  callback(null, {});
-}
+const locations = [];
 
 
 const server = new grpc.Server();
 
-server.addService(API.service, {
-  getConsumers,
-  getConsumer,
-  updateConsumer,
-  createConsumer,
-  removeConsumer,
-});
+const Consumer = {
+  async createConsumer({ request: { consumer } }) {
+    console.log('create', consumer);
+    const id = Math.max(0, ...consumers.map(c => c.id)) + 1;
+    consumers.push({ ...consumer, id });
+    return { result: { id } };
+  },
+  async readConsumer({ request: { id } }) {
+    console.log('read', id);
+    const consumer = consumers.find(c => c.id === id);
+    if (!consumer) return { error: 'ERROR_NOT_FOUND' }; // Error if not found
+    return { result: { consumer } };
+  },
+  async updateConsumer({ request: { consumer } }) {
+    console.log('update', consumer);
+    const current = consumers.find(c => c.id === consumer.id) || {};
+    Object.assign(current, consumer);
+    return { result: {} };
+  },
+  async deleteConsumer({ request: { id } }) {
+    console.log('delete', id);
+    const index = consumers.findIndex(c => c.id === id);
+    if (index !== -1) consumers.splice(index, 1);
+    return { result: {} };
+  },
+  async listConsumer({ request }) {
+    console.log('list', request);
+    return { result: { consumers } };
+  },
+};
+
+const Location = {
+  async createLocation({ request: { location } }) {
+    console.log('create', location);
+    const id = Math.max(0, ...locations.map(c => c.id)) + 1;
+    locations.push({ ...location, id });
+    return { result: { id } };
+  },
+  async readLocation({ request: { id } }) {
+    console.log('read', id);
+    const location = locations.find(c => c.id === id);
+    if (!location) return { error: 'ERROR_NOT_FOUND' }; // Error if not found
+    return { result: { location } };
+  },
+  async updateLocation({ request: { location } }) {
+    console.log('update', location);
+    const current = locations.find(c => c.id === location.id) || {};
+    Object.assign(current, location);
+    return { result: {} };
+  },
+  async deleteLocation({ request: { id } }) {
+    console.log('delete', id);
+    const index = locations.findIndex(c => c.id === id);
+    if (index !== -1) locations.splice(index, 1);
+    return { result: {} };
+  },
+  async listLocation({ request }) {
+    console.log('list', request);
+    return { result: { locations } };
+  },
+};
+
+server.addService(Api.service, callbackifyAll({
+  ...Consumer,
+  ...Location,
+}));
 
 server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
 server.start();
--- webapp/app.js
+++ webapp/app.js
@@ -1,28 +1,64 @@
-const bluebird = require('bluebird');
+const promisifyAll = require('util-promisifyall');
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 
 const protoFile = '../protos/api.proto';
 
-const packageDefinition = protoLoader.loadSync(protoFile);
-const { API } = grpc.loadPackageDefinition(packageDefinition).api;
+const protoOptions = {
+  keepCase: false,
+  longs: String,
+  enums: Number,
+  defaults: false,
+  oneofs: false,
+  arrays: true,
+};
+
+const packageDefinition = protoLoader.loadSync(protoFile, protoOptions);
+const { Api } = grpc.loadPackageDefinition(packageDefinition).api;
+
+const api = new Api('localhost:50051', grpc.credentials.createInsecure());
+
+promisifyAll(api);
 
 
-const api = new API('localhost:50051', grpc.credentials.createInsecure());
-
-bluebird.promisifyAll(api);
-
-
-async function demo() {
-  console.log(await api.createConsumerAsync({ name: 'Jan' }));
-  console.log(await api.getConsumersAsync({}));
-  console.log(await api.createConsumerAsync({ name: 'Klaas' }));
-  console.log(await api.getConsumersAsync({}));
-  console.log(await api.getConsumerAsync({ id: 2 }));
-  console.log(await api.updateConsumerAsync({ id: 2, name: 'Piet' }));
-  console.log(await api.getConsumerAsync({ id: 2 }));
-  console.log(await api.removeConsumerAsync({ id: 2 }));
-  console.log(await api.getConsumersAsync({}));
+function log(response) {
+  if (response.error) {
+    switch (response.error) {
+      case 'ERROR_ACTION_FAILED': console.log('Error', 'Action Failed'); break;
+      case 'ERROR_NOT_FOUND': console.log('Error', 'Not Found'); break;
+      default: break;
+    }
+  } else {
+    console.log('Result', response.result);
+  }
 }
 
-demo();
+async function demo() {
+  console.log('Manage consumers...');
+  await api.createConsumerAsync({ consumer: { name: 'Jan', type: 'COMPANY' } }).then(log);
+  await api.listConsumerAsync({}).then(log);
+  await api.createConsumerAsync({ consumer: { name: 'Klaas' } }).then(log);
+  await api.listConsumerAsync({}).then(log);
+  await api.readConsumerAsync({ id: 2 }).then(log);
+  await api.readConsumerAsync({ id: 20 }).then(log); // Not Exist
+  await api.updateConsumerAsync({ consumer: { id: 2, name: 'Piet' } }).then(log);
+  await api.readConsumerAsync({ id: 2 }).then(log);
+  await api.deleteConsumerAsync({ id: 2 }).then(log);
+  await api.listConsumerAsync({}).then(log);
+
+  console.log('Manage locations...');
+  await api.createLocationAsync({ location: { lat: 5, lon: 32 } }).then(log);
+  await api.listLocationAsync({}).then(log);
+  await api.createLocationAsync({ location: { lat: 7 } }).then(log);
+  await api.listLocationAsync({}).then(log);
+  await api.readLocationAsync({ id: 2 }).then(log);
+  await api.readLocationAsync({ id: 20 }).then(log); // Not Exist
+  await api.updateLocationAsync({ location: { id: 2, lon: 7 } }).then(log);
+  await api.readLocationAsync({ id: 2 }).then(log);
+  await api.deleteLocationAsync({ id: 2 }).then(log);
+  await api.listLocationAsync({}).then(log);
+}
+
+demo()
+  .then(() => console.log('success'))
+  .catch(err => console.error(err));
